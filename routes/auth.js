d6f9887b060201547bc7e6d4a7ac54dfa2ed49a5
Kunal Jiwane <Kunal.Jiwane@umassmed.edu>
2020-08-26 22:54:45 -0400

    Added comment

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const {User} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-
+//commmedted
 router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
